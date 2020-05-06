3ebe4930cba898810c287ad731e1ad8f0f1e550d
battleday_69 <rozbattleday@hotmail.com>
2020-05-06 15:13:04 -0700

    created eval files

--- eval_manager.py
+++ eval_manager.py
@@ -13,46 +13,10 @@ import numpy as np
 
 ### This script contains the main object class for training and optimization: TrainManager.
 #-----------------------------------
-# Helper functions
-def index_probabilities(teacherProbs, batch_idx):
-    """This function should batch the teacher probabilities for the L2 loss
-    using the torch backend.
 
-    Inputs
-    teacherProbs: numpy array(10000 x 10); model's output probs for validation set.
-    batch_idx: torch object: indices of images in batch being trained.
-
-    Outputs
-    teacherProbsBatched: torch object, teacher model output probabilities for batch
-    """
-    print('Not tested yet.')
-    teacherProbsBatched = teacherProbs[batch_idx.to(self.device).long()]
-    return teacherProbsBatched
-
-def custom_ce(input, target, reduction):
-    """Important: check this is the right ordering and where log applied.
-    target: distillation target. Should be given as probabilities.
-    input: model guess. Should be given as log probabilities."""
-    output = - torch.sum( target * input, dim=1)
-    if reduction == 'mean':
-        return torch.mean(output)
-
-class CEDivLoss(l._Loss):
-    """Custom loss class for cross-entropy between soft targets and output vectors.
-
-    Input: targets (distillation targets) as probabilities, input (model outputs) as log probabilities
-    """
-    __constants__ = ['reduction']
-
-    # use mean as default reduction
-    def __init__(self, size_average=None, reduce=None, reduction='mean'):
-        super(CEDivLoss, self).__init__(size_average, reduce, reduction)
-
-    def forward(self, input, target):
-        return custom_ce(input, target, reduction=self.reduction)
 #-----------------------------------
 # Main object class
-class TrainManager(object):
+class EvalManager(object):
     """TrainManager class. Will create environment to train and save a student network.
 
     Inputs (input through train_student.py)
@@ -68,136 +32,16 @@ class TrainManager(object):
     save: dump model and results after training in dictionary form using pytorch
     adjust_learning_rate: adjusts learning rate
     """
-    def __init__(self, student, teacher=None, train_loader=None, 
-        test_loader=None, train_config={}):
-        self.config = train_config
-        self.name = train_config['trial_id']
+    def __init__(self, student,  
+        test_loader=None, eval_config={}):
+        self.config = eval_config
+        self.name = self.config['trial_id']
         self.device = self.config['device']
         self.student = student
-        self.teacher_name = teacher['name']
-        self.teacher_model = teacher['model'].cuda()
-        self.have_teacher = bool(self.teacher_model)
-
-        # set teacher to correct mode (eval)
-        if self.have_teacher:
-            self.teacher_model.eval()
-            self.teacher_model.train(mode=False)
-        
-        self.train_loader = train_loader
+ 
         self.test_loader = test_loader
         # set up optimizer
-        self.optimizer = optim.SGD(self.student.parameters(),
-                                   lr=self.config['learning_rate'],
-                                   momentum=self.config['momentum'],
-                                   weight_decay=self.config['weight_decay'])
-
-        # set up scheduler
-        self.scheduler = ReduceLROnPlateau(self.optimizer, 'max', factor = 0.9, patience = 5, 
-            verbose = True)
-            
-    def train(self):
-        epochs = self.config['epochs']
-        trial_id = self.config['trial_id']
-        lambda_ = self.config['lambda_']
-        T_h = self.config['temperature_h']
-        T_t = self.config['temperature_t']
-        gamma_ = self.config['gamma_']
-
-        # to record losses afteer each training epoch; will be saved
-        validation_losses=[]
-        training_losses=[]
-        best_acc = 0
-        # L1 loss criterion
-        criterion = nn.CrossEntropyLoss()
-
-        # L2 loss criterion
-        if self.config['distil_fn'] == 'CE':
-            print('CE model')
-            distillation_criterion = CEDivLoss()
-        else:
-            print('{} model'.format(self.config['distil_fn']))
-            distillation_criterion = nn.KLDivLoss()
-
-        print('Starting student training, no = {} >>>>>>>>>>>>>'.format(trial_id))
-
-        for epoch in range(epochs):
-            # initialize torch model
-            self.student.train()
-
-            # loop over batches in training set (CIFAR10 validation set)
-            for batch_idx, (data, target_hard, target_soft) in enumerate(self.train_loader):
-
-                total_loss = 0
-                total_loss_SL = 0
-                total_loss_KD = 0
-                
-                # reset gradients
-                self.optimizer.zero_grad()
-
-                # organize and type data
-                data = data.to(self.device).float()
-                target_hard = target_hard.to(self.device).long() # may need this to survive
-                target_soft = target_soft.to(self.device).float() # may need this to survive
-                output = self.student(data)
-
-                # data loss
-                loss_SL = criterion(output, target_hard)
-                
-                # Knowledge Distillation loss
-                # return teacher targets by indexing into teacherProbs with batch_idx
-                if self.teacher_name == 'human':
-                    # should only be the case if learning from human labels, where teacherProbs is None
-                    teacher_outputs = target_soft
-                elif self.teacher_name == 'baseline':
-                    teacher_outputs = None
-                else:
-                    # IMPORTANT: CHECK THESE ARE PROBABILITIES: not here, but converted below
-                    teacher_outputs = self.teacher_model(data)
-
-                # if only training baseline teacher, use loss_SL (L1 loss) only
-                if self.teacher_name == 'baseline':
-                    loss_KD = loss_SL
-                # i.e., if only using human labels for loss
-                elif gamma_ == 1.0:
-                    loss_KD = T_h * T_h * distillation_criterion(F.log_softmax(output / T_h, dim=1),
-                                                 F.softmax(teacher_outputs / T_h, dim=1))
-                # i.e., if only using teacher labels for loss
-                elif gamma_ == 0.0:
-                    loss_KD = T_t * T_t * distillation_criterion(F.log_softmax(output / T_t, dim=1),
-                                                 F.softmax(teacher_outputs / T_t, dim=1))
-                # if using both human and teacher
-                else:
-                    # convex combination of teacher and human label loss
-                    loss_KD = T_t * T_t * (1-gamma_)*distillation_criterion(F.log_softmax(output / T_t, dim=1),
-                                                 F.softmax(teacher_outputs / T_t, dim=1)) + T_h * T_h * gamma_ * distillation_criterion(F.log_softmax(output / T_h, dim=1),
-                                                 F.softmax(target_soft / T_h, dim=1))
-                # total loss
-                loss = (1 - lambda_) * loss_SL + lambda_ * loss_KD
-                total_loss += loss
-                total_loss_SL += loss_SL
-                total_loss_KD += loss_KD
-                loss.backward()
-                self.optimizer.step()
-
-            # group training losses
-            training_losses.append((total_loss, total_loss_SL, total_loss_KD))
-            
-            # calculate validation losses
-            val_acc, val_loss = self.validate(step=epoch)
-            validation_losses.append((val_acc, val_loss))
-
-            print("epoch {}/{} done \n".format(epoch, epochs))          
-            print("training loss epoch>>",total_loss)
-            print("validation accuracy and loss of epoch>>",val_acc, val_loss)
-            
-            # only save if best epoch so far: if so, overwrite model
-            if val_acc > best_acc:
-                best_acc = val_acc
-                best_loss = val_loss
-                self.save(epoch, self.config['outfile'], training_losses, validation_losses)
-
-        return best_acc, best_loss
-    
+   
     def validate(self, step=0):
         self.student.eval()
         criterion = nn.CrossEntropyLoss()
@@ -222,34 +66,3 @@ class TrainManager(object):
             print('{{"metric": "{}_val_accuracy", "value": {}}}'.format(self.name, acc))
             return acc, total_val_loss
     
-    def save(self, epoch, name, training_losses, validation_losses):
-        trial_id = self.config['trial_id']
-        """will save model and parameters in path <name>"""
-        torch.save({
-                'model_state_dict': self.student.state_dict(),
-                'optimizer_state_dict': self.optimizer.state_dict(),
-                'epoch': epoch,
-                'training_losses': training_losses,
-                'validation_losses': validation_losses
-            }, name)
-
-    def adjust_learning_rate(self, optimizer, epoch):
-        epochs = self.config['epochs']
-        models_are_plane = self.config['is_plane']
-        
-        # depending on dataset
-        if models_are_plane:
-            lr = 0.01
-        else:
-            lr= self.config['learning_rate']
-            if epoch < int(epochs/2.0):
-                lr = 0.096
-            elif epoch < int(epochs*3/4.0):
-                lr = 0.096 * 0.1
-            else:
-                lr = 0.096 * 0.01
-        
-        print('Learning rate >>>',lr)
-        # update optimizer's learning rate
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
--- eval_student.py
+++ eval_student.py
@@ -6,7 +6,7 @@ import torch
 import argparse
 from data_loader import get_cifar
 from find_best_teacher import *
-from train_manager import TrainManager
+from eval_manager import EvalManager
 from model_factory import create_cnn_model, is_resnet
 
 def str2bool(v):
@@ -27,29 +27,16 @@ def load_checkpoint(model, checkpoint_path):
     return model
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Training KD Teachers Code')
+    parser = argparse.ArgumentParser(description='Evaluating student generalization Code')
     parser.add_argument('--master_outdir', default='', type=str, help='model dump dir')
-    parser.add_argument('--teacher', default='human', type=str, help='teacher name')
     parser.add_argument('--student', default='resnet8', type=str, help='student name')
     parser.add_argument('--manual_seed', default=0, type=int, help='manual seed')
-    parser.add_argument('--iter', default=0, type=int, help='run # with same parameters')
 
-    parser.add_argument('--temperature_t', default=1.0, type=float,  help='student teacher temperature')
-    parser.add_argument('--temperature_h', default=1.0, type=float,  help='student human temperature')
-    parser.add_argument('--lambda_', default=0.5, type=float,  help='weighted average')
-    parser.add_argument('--gamma_', default=0.0, type=float,  help='weighted average')
-    parser.add_argument('--distil_fn', default='KD', type=str,  help='for distillation loss (KD or CE)')
-
-    parser.add_argument('--epochs', default=500, type=int,  help='number of total epochs to run')
     parser.add_argument('--dataset', default='cifar10', type=str, help='dataset. can be either cifar10 or cifar100')
     parser.add_argument('--batch-size', default=128, type=int, help='batch_size')
-    parser.add_argument('--learning-rate', default=0.1, type=float, help='initial learning rate')
-    parser.add_argument('--momentum', default=0.9, type=float,  help='SGD momentum')
-    parser.add_argument('--weight-decay', default=1e-4, type=float, help='SGD weight decay (default: 1e-4)')
-    
+
     parser.add_argument('--cuda', default=False, type=str2bool, help='whether or not use cuda(train on GPU)')
     parser.add_argument('--dataset-dir', default='./data', type=str,  help='dataset directory')
-    parser.add_argument('--trial_id', default='', type=str,  help='id string')
     
     args = parser.parse_args()
     return args
@@ -61,26 +48,6 @@ if __name__ == "__main__":
 
     # prepare paths and log
     # student results will be saved under their teacher's superdirectory
-    teacher_details = args.teacher.split('_')
-    teacher_name = '_'.join(teacher_details[:-1])
-    if 'shake26' in args.teacher:
-        teacher_arch = 'shake26'
-    elif 'resnet8' in args.teacher:
-        teacher_arch = 'resnet8'
-    else:
-        print('something wrong with teacher input in train_student.py')
-    teacher_args = teacher_details[-1]
-
-
-    save_path = '{}/{}_{}/{}'.format(args.master_outdir, teacher_name, teacher_args, args.student)
-    log_path = '{}/training_log.log'.format(save_path)
-    print('saving model run in {} \n'.format(save_path))
-
-    if not os.path.exists(save_path):
-            print('making new dirs')
-            os.makedirs(save_path)
-
-    logs = open(log_path, 'a')
 
     # set seed
     torch.manual_seed(args.manual_seed)
@@ -92,57 +59,26 @@ if __name__ == "__main__":
         num_classes = 10 
     else:
         print('cifar10 not loaded!')
+        return
 
-    # dataloaders for training paradigm
-    train_loader, test_loader = get_cifar(num_classes, batch_size=args.batch_size, crop=True)
+    # dataloaders for training paradigm --- do all three for generalization
+    _, test_loader = get_cifar(num_classes, batch_size=args.batch_size, crop=True)
 
     # prepare train_config, to be passed into TrainManager class
-    train_config = {
-                'epochs': args.epochs,
-                'learning_rate': args.learning_rate,
-                'momentum': args.momentum,
-                'weight_decay': args.weight_decay,
+    eval_config = {
                 'device': 'cuda' if args.cuda else 'cpu',
                 'trial_id': args.trial_id,
-                'batch_size': args.batch_size,
-                'distil_fn': args.distil_fn,
-                'lambda_': args.lambda_,
-                'temperature_h': args.temperature_h,
-                'temperature_t': args.temperature_t,
-                'gamma_': args.gamma_
+                'batch_size': args.batch_size
             }
 
     # create student model for CIFAR10; usually shake26 for initial student and resnet8 thereafter.
-    student_model = create_cnn_model(args.student, dataset, use_cuda=args.cuda)
-
-    # below will be dict with teacher name and probs for CIFAR10 validation subset
-        
-    teacher = {'name':teacher_name, 'arch': teacher_arch, 'args': teacher_args}
-    print('teacher is: {}'.format(teacher))
-    teacher_model = load_best_model(teacher, args.master_outdir, cuda_option = args.cuda)
-    teacher['model'] = teacher_model
-    # unique identifier
-    student_name = 'student_{0}_distil_fn_{1}_temperature_h_{2}_temperature_t_{3}_lambda_{4}_gamma_{5}_iter_{6}_best.pth.tar'.format(args.student, 
-                                args.distil_fn, args.temperature_h, args.temperature_t, args.lambda_, args.gamma_, args.iter)
+    student_model = load_teacher_model(arg.student_path, 'resnet8', dataset, use_cuda=args.cuda)
 
     # where to dump final model
-    train_config['outfile'] = '{}/{}'.format(save_path, student_name)
-
-    print("---------- Training Student -------")
     
-    student_trainer = TrainManager(student_model, teacher, 
-                                   train_loader=train_loader, test_loader=test_loader, train_config=train_config)
+    print("---------- Evaluating Student -------")
+    student_trainer = EvalManager(student_model, test_loader=test_loader, eval_config=eval_config)
 
-    best_valacc, best_valloss = student_trainer.train()
-
-    print("Best student accuacy for teacher {0}, student {1}, distil_fn {2}, temp t {3}, temp_h {4}, lambda {5}, gamma {6}, iter {7} is {8}".format(teacher['name'],
-        args.student, args.distil_fn, args.temperature_t, args.temperature_h, args.lambda_, args.gamma_, args.iter, best_valacc))
+    best_valacc, best_valloss = student_trainer.validate()
 
 
-    logline = "teacher {0}, student {1}, distil_fn {2}, temp t {3},  temp h {4}, lambda {5}, gamma {6}, iter {7},valacc {8}, valloss {9}, \n".format(teacher['name'],
-              args.student, args.distil_fn, args.temperature_t, args.temperature_h, args.lambda_, args.gamma_, args.iter, best_valacc, best_valloss)
-
-    logs.write(logline)
-    logs.flush()
-
-    os.fsync(logs.fileno())
